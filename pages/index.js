f8778a5a9a388e5225906701c90e0e80ffcaa28a
Avineshrv <avineshkumar.r@c.hasbro.com>
2024-09-20 12:06:55 +0530

    test

--- pages/index.js
+++ pages/index.js
@@ -1,115 +1,142 @@
-import Image from "next/image";
-import localFont from "next/font/local";
+import React, { useState } from 'react';
 
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
+const initialData = [
+  {
+    "id": "electronics",
+    "label": "Electronics",
+    "value": 1400, //this value needs to be calculated from the children values (800+700)
+    "variance": 0,
+    "children": [
+      {
+        "id": "phones",
+        "label": "Phones",
+        "value": 800,
+        "variance": 0,
+      },
+      {
+        "id": "laptops",
+        "label": "Laptops",
+        "value": 700,
+        "variance": 0,
+      }
+    ]
+  },
+  {
+    "id": "furniture",
+    "label": "Furniture",
+    "value": 1000, //this need to be calculated from the children values (300+700)
+    "variance": 0,
+    "children": [
+      {
+        "id": "tables",
+        "label": "Tables",
+        "value": 300,
+        "variance": 0,
+      },
+      {
+        "id": "chairs",
+        "label": "Chairs",
+        "value": 700,
+        "variance": 0,
+      }
+    ]
+  }
+]
 
-export default function Home() {
+const App = () => {
+  const [data, setData] = useState(initialData);
+  const [percent, setPercent] = useState(0)
+
+  const handlePercentage = (path, percent) => {
+    const newData = [...data];
+    let current = newData;
+    path.forEach((index, i) => {
+      // current[index].value += (current[index].value + percent);
+      if (i === path.length - 1) {
+        current[index].value += (current[index].value * percent) / 100;
+        current[index].variance = percent
+      } else {
+        current = current[index].children;
+      }
+    });
+    setData(newData);
+  };
+
+  const handleValue = (path, percent) => {
+    const newData = [...data];
+    let current = newData;
+    path.forEach((index, i) => {
+      console.log(current[index], "-----------------------ssssss");
+      console.log(path.length,i, "-----------------------ssssss");
+      
+      if (i === path.length - 1) {
+        current[index].value += (current[index].value * percent) / 100;
+        current[index].variance = (current[index].variance + percent) / percent * 100
+        console.log("IF WOKING");
+        
+      } else {
+        current = current[index].children;
+        console.log("ELSE WORKING");
+        
+      }
+    });
+    setData(newData);
+  }
+  
+
+  const renderRows = (rows, path = []) => {
+    return rows.map((row, index) => {
+      console.log(row, "ROW");
+      
+      const currentPath = [...path, index];
+
+      console.log(currentPath, "CURRENT PATH");
+      
+      return (
+        <React.Fragment key={row.label}>
+          <tr className="border border-white">
+            <td className="p-2 border border-white">{path.length === 3 ? `--${row.label}` : row.label}</td>
+            <td className="p-2 border border-white">{row.value}</td>
+            <td className="p-2 border border-white">
+              <input
+                className="text-black"
+                type="number"
+                onChange={(e) => setPercent(Number(e.target.value))}
+              />
+            </td>
+            <td className="p-2 border border-white">
+              <button className="bg-blue-300 text-black px-2 py-1 mx-auto w-full" onClick={() => handlePercentage(currentPath, percent)}>percent</button>
+            </td>
+            <td className="p-2 border border-white">
+              <button className="bg-blue-300 px-2 py-1 text-black mx-auto w-full" onClick={() => handleValue(currentPath, percent)}>Value</button>
+            </td>
+            <td className="p-2 border border-white">{row.variance}%</td>
+          </tr>
+          {row.children && renderRows(row.children, currentPath)}
+        </React.Fragment>
+      );
+    });
+  };
+
   return (
-    <div
-      className={`${geistSans.variable} ${geistMono.variable} grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]`}
-    >
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              pages/index.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+    <div>
+      <table className="border-2 border-white">
+        <thead>
+          <tr>
+            <th className="p-3 border border-white">Label</th>
+            <th className="p-3 border border-white">Value</th>
+            <th className="p-3 border border-white">Input</th>
+            <th className="p-3 border border-white">Allocation %</th>
+            <th className="p-3 border border-white">Allocation Val</th>
+            <th className="p-3 border border-white">Variance %</th>
+          </tr>
+        </thead>
+        <tbody className="border-white border">
+          {renderRows(data)}
+        </tbody>
+      </table>
     </div>
   );
-}
+};
+
+export default App;
